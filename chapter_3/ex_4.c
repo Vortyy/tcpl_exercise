0ae18f34ddb352fbf20fbd03264c2dab0265f5af
vorty <uf4vortex@gmail.com>
2024-12-03 22:59:44 +0100

    chapter 3 ended

--- chapter_3/ex_4.c
+++ chapter_3/ex_4.c
@@ -41,12 +41,58 @@ void itoa(int n, char s[]){
   reverse(s);
 }
 
+//Here you can find the math insight about this function :
+//https://math.stackexchange.com/questions/1359770/why-does-the-division-algorithm-work-for-converting-between-number-bases
+void itob(int n, char s[], int b){
+  int sign, i;
+  sign = n;
+  i = 0;
+  do {
+    int d = abs(n % b);
+    s[i++] = (d > 9) ? d - 10 + 'a' : d + '0';
+  } while ((n /= b) != 0);
+  
+  if(sign < 0)
+    s[i++] = '-';
+
+  s[i] = '\0';
+
+  reverse(s);
+}
+
+//Where p represent a padding 
+//small notice here C doesn't support overloading then it's impossible to set a 2 methodes with the same name even if the signature differt
+void itoa_v2(int n, char s[], int p){
+  int sign, i;
+  sign = n;
+  i = 0;
+  do {
+    s[i++] = abs(n % 10) + '0';
+  } while ((n /= 10) != 0);
+
+  if(sign < 0)
+    s[i++] = '-';
+
+  while(i < p)
+    s[i++] = ' ';
+
+  s[i] = '\0';
+  reverse(s);
+}
+
 int main(int argc, char *argv[])
 {
+  int t = 4902;
   char seq[100];
   
   itoa(INT_MIN, seq);
   printf("%d : %s \n", INT_MIN, seq);
+
+  itob(t, seq, 16);
+  printf("%d : %s \n", t, seq);
+
+  itoa_v2(t, seq, 8);
+  printf("%d : %s \n", t, seq);
 
   return EXIT_SUCCESS;
 }
