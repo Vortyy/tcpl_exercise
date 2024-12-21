ada6bf7c224b526be5bcda3d73a3e9b9ec064a49
vorty <uf4vortex@gmail.com>
2024-12-22 00:21:52 +0100

    ex_6 done

---
+++ chapter_5/ex_6.c
@@ -0,0 +1,47 @@
+/************************************************************************************
+ * The C Programming Language 5-6:
+ * 
+ * -> write a function htoi(s) -> take a string of hex decimal value and convert it
+ *    to its equivalent in integer value
+ *
+ * Copyright (c) 2024 CHABOT Yohan 
+ ************************************************************************************/
+
+#include <ctype.h>
+#include <stdio.h>
+
+#define MAXLINE 100
+
+int get_line(char *s, int lim){
+  int c;
+  char *t = s; /* starting point length */
+  while(--lim > 0 && (c = getchar()) != EOF && c != '\n')
+    *s++ = c;
+  *s = '\0';
+
+  //printf("size %ld \n", (s - t));
+  return s - t;
+}
+
+int atoi_(char *s){
+  int n = 0;
+  int sign = 1;
+  
+  if(*s == '-'){
+    sign = -1;
+    s++;
+  }
+
+  for(; isdigit(*s); s++)
+    n = 10 * n + (*s - '0');
+
+  return n * sign;
+}
+
+int main(){
+  char line[MAXLINE];
+  while(get_line(line, MAXLINE) > 0){
+    printf("%d\n", atoi_(line));
+  } 
+  return 0;
+}
