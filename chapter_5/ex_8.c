8b2434cedde177216fcfd8d8f4b6445a37488465
vorty <uf4vortex@gmail.com>
2024-12-22 23:10:36 +0100

    ex 8 done

--- chapter_5/ex_8.c
+++ chapter_5/ex_8.c
@@ -8,8 +8,9 @@
  ************************************************************************************/
 
 #include <stdio.h>
+#include <sys/types.h>
 
-#define isLeap(x) (x%4 == 0 && x%100 != 0 || year%400 == 0)
+#define isLeap(x) (x%4 == 0 && x%100 != 0 || year%400 == 0) /* a leap year is every 4 year except every 100 year and 400 */
 
 static char daytab[2][13] = {
   {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
@@ -21,6 +22,8 @@ int day_of_year(int year, int month, int day)
 {
   int i, leap;
   leap = isLeap(year); 
+  if(month < 1 || month > 12 || day < 1 || day > daytab[leap][month])
+    return -1;
   for (i = 1; i < month; i++)
     day += daytab[leap][i];
   return day;
@@ -32,15 +35,22 @@ void month_day(int year, int yearday, int *pmonth, int *pday)
   leap = isLeap(year);
   for (i = 1; i <= 12 && yearday > daytab[leap][i]; i++)
     yearday -= daytab[leap][i];
-  *pmonth = i;
-  *pday = yearday;
+
+  if(i > 12){
+    *pday = -1;
+    *pmonth = -1;
+  }else{
+    *pmonth = i;
+    *pday = yearday;
+  }
 }
 
 int main(){
   int pday, pmonth;
-  int year = 2000;
-  int yearday = 196;
+  int year = 1999;
+  int yearday = 365;
 
   month_day(year, yearday, &pmonth, &pday);
   printf("day %d of year %d is %d/%d/%d\n", yearday, year, pday, pmonth, year);
+  printf(" --> %d <--\n", day_of_year(year, 7, 19));
 }
