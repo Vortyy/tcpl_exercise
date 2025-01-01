554b5a5d0d232b1a19cefbdb1ef48e2e0f41e3bd
vorty <uf4vortex@gmail.com>
2025-01-01 22:09:27 +0100

    ex_20 done

--- chapter_5/ex_20.c
+++ chapter_5/ex_20.c
@@ -11,10 +11,12 @@
 
 #define MAXTOKEN 100
 
-enum { ERROR, NAME, PARENS, BRACKETS };
+enum { ERROR, QUALIFIER, TYPE, NAME, PARENS, BRACKETS };
 
-void dcl(void);
-void dirdcl(void);
+void extdcl(void);   
+void paramdcl(void);
+void dcl(int);
+void dirdcl(int);
 int gettoken(void);
 
 int getch();
@@ -27,52 +29,106 @@ int tokentype;            /* type of last token */
 char token[MAXTOKEN];     /* last token string */
 char name[MAXTOKEN];      /* identifier name */
 char datatype[MAXTOKEN];  /* data type = char, int, etc. */
+char qualifier[MAXTOKEN]; /* qualifier = const, register */
 char out[1000];
 
 char error;               /* error marker */
 
-/* in this answer i choose to skip the line when it got an unexpected input */
+/* we choose to modify */
 int main(){
   error = 0;
   while(gettoken() != EOF){
-    strcpy(datatype, token);
     out[0] = '\0';
-    dcl();
-    if(tokentype != '\n')
-      dcl_error("syntax error");
+     
+    extdcl();
 
     if(error) /* if an error append during dcl execution */
       clear();
-    else
-      printf("%s: %s %s \n", name, out, datatype);
+    else {
+      printf("%s :%s ", name, out);
+      if(qualifier[0] != '\0')
+        printf("%s ", qualifier);
+      printf("%s\n", datatype);
+    }
   }
   return 0;
 }
 
-void dcl(){
+void extdcl(){
+  //case QUALIFIE
+  if(tokentype == QUALIFIER){
+    strcpy(qualifier, token);
+    gettoken();
+  }
+
+  if(tokentype != TYPE)
+    dcl_error("no data type");
+  else{
+    int type;
+    strcpy(datatype, token);
+    dcl(0); /* not a param */
+  }
+}
+
+void paramdcl(){
+  int type;
+  char temp[MAXTOKEN];
+  temp[0] = '\0';
+
+  gettoken(); 
+  if(tokentype == QUALIFIER){
+    strcat(temp, token);
+    gettoken();
+  }
+    
+  if(tokentype != TYPE){
+    dcl_error("param got no type");
+  } else {
+    strcat(temp, " ");
+    strcat(temp, token);
+    dcl(1); /* param */
+  }
+
+  if(!error)
+    strcat(out, temp);
+}
+
+void dcl(int isparam){
   int ns;
   for(ns = 0; gettoken() == '*';ns++)
     ;
-  dirdcl();
+  dirdcl(isparam);
   while(ns-- > 0)
     strcat(out, " pointer to");
 }
 
-void dirdcl(){
+void dirdcl(int isparam){
   int type;
 
   if(tokentype == '('){
-    dcl();
+    dcl(isparam);
     if(tokentype != ')')
       dcl_error("missing ) ...");
-  } else if (tokentype == NAME) 
-    strcpy(name, token);
+  } else if (tokentype == NAME){
+    if(name[0] == '\0')
+      strcpy(name, token);
+  } else if (isparam && (tokentype == ')' || tokentype == ','))
+    return;
   else
-    dcl_error("expected name or (dcl)");
+    if(!isparam)
+      dcl_error("expected name or (dcl)");
 
-  while((type=gettoken()) == PARENS || type == BRACKETS)
+  while((type=gettoken()) == PARENS || type == BRACKETS || type == '(')
     if(type == PARENS)
       strcat(out, " function returning");
+    else if(type == '('){
+      strcat(out, " function expecting");
+      while(tokentype != ')' && tokentype != ERROR){
+        paramdcl();
+        strcat(out, " and");
+      }
+      strcat(out, " returning");
+    }
     else{
       strcat(out, " array");
       strcat(out, token);
@@ -85,7 +141,7 @@ int gettoken(){
   char *p = token;
 
   if(error)
-    return ERROR;
+    return tokentype = ERROR;
 
   while((c = getch()) == ' ' || c == '\t');
 
@@ -106,6 +162,13 @@ int gettoken(){
       *p++ = c;
     *p = '\0';
     ungetch(c);
+
+    if(!strcmp(token, "const") || !strcmp(token, "register"))
+      return tokentype = QUALIFIER;
+
+    if(!strcmp(token, "int") || !strcmp(token, "char")) /* now we support only 2 types but can be improve by setting a list of type and apply a bsearch inside that list */
+      return tokentype = TYPE;
+
     return tokentype = NAME;
   } else 
     return tokentype = c;
@@ -123,8 +186,9 @@ int bufp = 0;         /* next free position in buf */
 
 void clear(){ /* reset buffer reader */
   printf("There was at least 1 error in your previous input please retype it :\n");
-  bufp = 0; /* resest getch */
   error = 0;
+  while(gettoken() != '\n');
+  bufp = 0; /* resest getch */
 }
 
 int getch(){    /* get characters maybe a pushed-back one */
